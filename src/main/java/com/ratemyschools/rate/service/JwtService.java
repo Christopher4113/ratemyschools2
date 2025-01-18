2c1b6794e6a28c8b7b346e178bbc6fc817a9e7b3
Christopher4113 <ch.lam1328@gmail.com>
2025-01-17 19:44:51 -0500

    hopefully done

--- src/main/java/com/ratemyschools/rate/service/JwtService.java
+++ src/main/java/com/ratemyschools/rate/service/JwtService.java
@@ -54,7 +54,7 @@ public class JwtService {
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + expiration))
-                .signWith(getSignInKey(), SignatureAlgorithm.ES256)
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
     public boolean isTokenValid(String token, UserDetails userDetails) {
@@ -63,7 +63,7 @@ public class JwtService {
     }
 
     private boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
+        return extractExpiration(token).after(new Date());
     }
 
     private Date extractExpiration (String token) {
